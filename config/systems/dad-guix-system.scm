c75a3c7c35ed1d1a7f293a0104fa2be5eff4a5d7
Brian Tomlinson <darthlukan@gmail.com>
2026-03-08 05:30:54 -0700

    Fix bad paren matching
    Caused `guix system reconfigure` to error with `services` requires a
    list of services or somesuch.

--- config/systems/dad-guix-system.scm
+++ config/systems/dad-guix-system.scm
@@ -1,9 +1,17 @@
 (use-modules (gnu)
+             (gnu services)
+             (guix)
              (nongnu packages linux)
              (nongnu system linux-initrd)
-             (gnu system accounts))
+             (gnu system accounts)
+             (gnu system nss))
 (use-package-modules wm)
-(use-service-modules containers cups desktop networking ssh xorg)
+(use-service-modules containers
+                     cups
+                     desktop
+                     networking
+                     ssh
+                     xorg)
 
 (operating-system
  (locale "en_US.utf8")
@@ -13,101 +21,7 @@
  (timezone "America/Los_Angeles")
  (keyboard-layout (keyboard-layout "us"))
  (host-name "dad-guix")
- (groups (cons*
-          (user-group
-           (name "pipewire")
-           (system? #t))
-          (cons*
-           (user-group
-            (name "cgroup")
-            (system? #t))
-           %base-groups)))
-
- ;; The list of user accounts ('root' is implicit).
- (users (cons* (user-account
-                (name "dad")
-                (comment "Brian Tomlinson")
-                (group "users")
-                (home-directory "/home/dad")
-                (supplementary-groups '("wheel" "netdev" "audio" "video"
-                                        "dialout" "disk" "pipewire" "input"
-                                        "cgroup")))
-               %base-user-accounts))
-
- ;; Packages installed system-wide.  Users can also install packages
- ;; under their own account: use 'guix search KEYWORD' to search
- ;; for packages and 'guix install PACKAGE' to install a package.
- (packages
-  (append
-   (specifications->packages
-    (list "sway"
-          "sbcl"
-          "stumpwm"
-          "font-dejavu"
-          "wmenu"
-          "foot"
-          "qutebrowser"
-          "git"
-          "vim"
-          "vim-guix-vim"
-          "fd"
-          "shellcheck"
-          "markdown"
-          "emacs"
-          "emacs-guix"
-          "ranger"
-          "pcmanfm"
-          "conmon"
-          "netavark"
-          "podman"
-          "podman-compose"))
-   %base-packages))
-
- ;; Below is the list of system services.  To search for available
- ;; services, run 'guix system search KEYWORD' in a terminal.
- (services
-  (append (list
-           ;; To configure OpenSSH, pass an 'openssh-configuration'
-           ;; record as a second argument to 'service' below.
-           (service openssh-service-type)
-           (service cups-service-type)
-           (set-xorg-configuration
-            (xorg-configuration (keyboard-layout keyboard-layout))))
-
-          ;; This works because it ultimately returns a list of services by
-          ;; way of modifying %DESKTOP-SERVICES, which is a list of services.
-          ;; Include `nonguix` substitutes.
-          (modify-services %desktop-services
-                           (guix-service-type
-                            config =>
-                            (guix-configuration
-                             (inherit config)
-                             (substitute-urls
-                              (append
-                               (list
-                                "https://substitutes.nonguix.org"
-                                "https://cache-us-lax.guix.moe")
-                               %default-substitute-urls))
-                             (authorized-keys
-                              (append
-                               (list
-                                (local-file
-                                 "./nonguix-signing-key.pub")
-                                (plain-file "guix-moe-old.pub"
-                                            "(public-key (ecc (curve Ed25519) (q #374EC58F5F2EC0412431723AF2D527AD626B049D657B5633AAAEBC694F3E33F9#)))")
-                                ;; New key since 2025-10-29.
-                                (plain-file "guix-moe.pub"
-                                            "(public-key (ecc (curve Ed25519) (q #552F670D5005D7EB6ACF05284A1066E52156B51D75DE3EBD3030CD046675D543#)))"))
-                               %default-authorized-guix-keys)))))
-
-          (service rootless-podman-service-type
-                   (rootless-podman-configuration
-                    (subgids
-                     (list (subid-range (name "dad"))))
-                    (subuids
-                     (list (subid-range (name "dad"))))))))
-
-
+ ;; Bootloader config
  (bootloader (bootloader-configuration
               (bootloader grub-efi-bootloader)
               (targets (list "/boot/efi"))
@@ -119,11 +33,85 @@
  ;; by running 'blkid' in a terminal.
  (file-systems (cons* (file-system
                        (mount-point "/")
-                       (device (uuid "3527a79f-ff4c-4a8f-a11e-188ced67539d"
-                                     'ext4))
+                       (device (uuid "3527a79f-ff4c-4a8f-a11e-188ced67539d" 'ext4))
                        (type "ext4"))
                       (file-system
                        (mount-point "/boot/efi")
-                       (device (uuid "5894-B18E"
-                                     'fat32))
-                       (type "vfat")) %base-file-systems)))
+                       (device (uuid "5894-B18E" 'fat32))
+                       (type "vfat")) %base-file-systems))
+ ;; Additional Groups for our user
+ (groups (cons* (user-group
+                 (name "pipewire")
+                 (system? #t))
+                (cons* (user-group
+                        (name "cgroup")
+                        (system? #t)) %base-groups)))
+ ;; The list of user accounts ('root' is implicit).
+ (users (cons* (user-account
+                (name "dad")
+                (comment "Brian Tomlinson")
+                (group "users")
+                (home-directory "/home/dad")
+                (supplementary-groups '("wheel"
+                                        "netdev"
+                                        "audio"
+                                        "video"
+                                        "dialout"
+                                        "disk"
+                                        "pipewire"
+                                        "input"
+                                        "cgroup")))
+               %base-user-accounts))
+
+ ;; Packages installed system-wide.  Users can also install packages
+ ;; under their own account: use 'guix search KEYWORD' to search
+ ;; for packages and 'guix install PACKAGE' to install a package.
+ (packages (append (specifications->packages (list "sway"
+                                                   "sbcl"
+                                                   "stumpwm"
+                                                   "font-dejavu"
+                                                   "wmenu"
+                                                   "foot"
+                                                   "qutebrowser"
+                                                   "git"
+                                                   "vim"
+                                                   "vim-guix-vim"
+                                                   "fd"
+                                                   "shellcheck"
+                                                   "markdown"
+                                                   "emacs"
+                                                   "emacs-guix"
+                                                   "ranger"
+                                                   "pcmanfm"
+                                                   "conmon"
+                                                   "netavark"
+                                                   "podman"
+                                                   "podman-compose"))
+                   %base-packages))
+ ;; Below is the list of system services.  To search for available
+ ;; services, run 'guix system search KEYWORD' in a terminal.
+ (services
+  (append
+   (list
+    (service openssh-service-type)
+    (service cups-service-type)
+    (set-xorg-configuration
+     (xorg-configuration (keyboard-layout keyboard-layout)))
+    (service rootless-podman-service-type
+             (rootless-podman-configuration
+              (subgids (list (subid-range (name "dad"))))
+              (subuids (list (subid-range (name "dad")))))))
+   (modify-services %desktop-services
+                    (guix-service-type config =>
+                                       (guix-configuration
+                                        (inherit config)
+                                        (substitute-urls (append (list
+                                                                  ;; "https://substitutes.nonguix.org"
+                                                                  "https://cache-us-lax.guix.moe")
+                                                                 %default-substitute-urls))
+                                        (authorized-keys (append (list
+                                                                  (local-file "./nonguix-signing-key.pub")
+                                                                  (plain-file "guix-moe-old.pub" "(public-key (ecc (curve Ed25519) (q #374EC58F5F2EC0412431723AF2D527AD626B049D657B5633AAAEBC694F3E33F9#)))")
+                                                                  (plain-file "guix-moe.pub" "(public-key (ecc (curve Ed25519) (q #552F670D5005D7EB6ACF05284A1066E52156B51D75DE3EBD3030CD046675D543#)))"))
+                                                                 %default-authorized-guix-keys)))))))
+ (name-service-switch %mdns-host-lookup-nss))
